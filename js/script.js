9ea8b6fcb93defae53f9cd96704ff5e6093838c3
Roman <kurpelroma@gmail.com>
2023-11-24 13:32:27 +0200

    edits

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 const menuBody = document.querySelector('.menu__body');
 // Event listeners
 document.addEventListener('click', (e) => {
-    if (e.target.classList.contains('anchor')) {
+    if (e.target.classList.contains('anchor') || e.target.classList.contains('buy__btn')) {
         e.preventDefault()
-        anchorClick(e.target)
+        e.target.classList.contains('buy__btn') ? anchorClick(e.target.parentElement) : anchorClick(e.target)
     }
     if (e.target.classList.contains('header__burger')) {
         e.preventDefault()
@@ -23,17 +23,17 @@ function anchorClick(e) {
     const w = window.pageYOffset
 
     const blockId = e.getAttribute('href').substring(1),
-     scrollTarget = document.getElementById(blockId),
-     t = scrollTarget.getBoundingClientRect().top
-     start = null
+        scrollTarget = document.getElementById(blockId),
+        t = scrollTarget.getBoundingClientRect().top
+    start = null
 
-     requestAnimationFrame(step)
+    requestAnimationFrame(step)
 
-     function step(time) {
+    function step(time) {
         if (start == null) start = time
 
         let progress = time - start,
-        r = (t < 0 ? Math.max(w - progress/v, w + t): Math.min(w + progress/v, w + t))
+            r = (t < 0 ? Math.max(w - progress / v, w + t) : Math.min(w + progress / v, w + t))
 
         window.scrollTo(0, r)
 
@@ -42,19 +42,24 @@ function anchorClick(e) {
         } else {
             location.hash = '#' + blockId;
         }
-     }
+    }
 }
+
+
 function toggleMenu() {
-  const btn = document.querySelector('.header__burger');
-  menuBody.classList.toggle('active');
-  btn.classList.toggle('active');
-  btn.classList.contains('active') ? document.body.classList.add('scroll--block') : document.body.classList.remove('scroll--block')
+    const btn = document.querySelector('.header__burger');
+    menuBody.classList.toggle('active');
+    btn.classList.toggle('active');
+    btn.classList.contains('active') ? document.body.classList.add('scroll--block') : document.body.classList.remove('scroll--block')
 }
 
 const acc = document?.querySelectorAll('.accordeon');
-for (let i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-  acc[i].querySelector('.moduls__accordeon-btn').classList.toggle("active")
-  acc[i].querySelector('.panel').classList.toggle("panelActive")
-  });
+
+if (acc) {
+    for (let i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            acc[i].querySelector('.moduls__accordeon-btn').classList.toggle("active")
+            acc[i].querySelector('.panel').classList.toggle("panelActive")
+        });
+    }
 }
